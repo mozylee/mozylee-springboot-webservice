dcfda3a5d4f5fadf13f6596dfea4327cf072ba70
Mozy Lee <whyalwaysmeyy@gmail.com>
2022-07-26 00:02:52 +0900

    4.3 게시글 등록 화면 만들기
    억지로 () => {} 쓰니까 바인딩 이상해져서 save를 인식 못하는 문제가 있었음. 그래서 얌전히 function 형태로 재수정

--- src/main/java/com/mozylee/book/springboot/web/IndexController.java
+++ src/main/java/com/mozylee/book/springboot/web/IndexController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
