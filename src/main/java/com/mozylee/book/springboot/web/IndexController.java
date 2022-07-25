6e8ff1fdc89f939734fba3fb5416d6bdefae62e8
Mozy Lee <whyalwaysmeyy@gmail.com>
2022-07-26 00:18:33 +0900

    4.4 전체 조회 화면 만들기
    model 활용해서 attribute 넣는거 뭔가 익숙한데... 어디선가 해봤던거 같다.

--- src/main/java/com/mozylee/book/springboot/web/IndexController.java
+++ src/main/java/com/mozylee/book/springboot/web/IndexController.java
@@ -1,12 +1,19 @@
 package com.mozylee.book.springboot.web;
 
+import com.mozylee.book.springboot.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsRepository postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
