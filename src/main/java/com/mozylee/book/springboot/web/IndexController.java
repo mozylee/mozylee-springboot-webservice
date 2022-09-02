acce4c433830e5a9c27e55e29017afcd6dc2c8e4
Mozy Lee <whyalwaysmeyy@gmail.com>
2022-09-03 01:02:17 +0900

    5.3 구글 로그인 연동하기 - 화면에 로그인 추가

--- src/main/java/com/mozylee/book/springboot/web/IndexController.java
+++ src/main/java/com/mozylee/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.mozylee.book.springboot.web;
 
+import com.mozylee.book.springboot.config.auth.dto.SessionUser;
 import com.mozylee.book.springboot.domain.posts.Posts;
 import com.mozylee.book.springboot.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsRepository postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
