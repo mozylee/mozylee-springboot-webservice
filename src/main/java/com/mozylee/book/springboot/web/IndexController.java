4197763aecaeaa58a30efc1b8f08a0f38f606851
Mozy Lee <whyalwaysmeyy@gmail.com>
2022-09-03 09:37:13 +0900

    5.4 어노테이션 기반으로 개선
    - 메서드에서 SessionUser를 가져오는 부분을 어노테이션 기반으로 수정

--- src/main/java/com/mozylee/book/springboot/web/IndexController.java
+++ src/main/java/com/mozylee/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.mozylee.book.springboot.web;
 
+import com.mozylee.book.springboot.config.auth.LoginUser;
 import com.mozylee.book.springboot.config.auth.dto.SessionUser;
 import com.mozylee.book.springboot.domain.posts.Posts;
 import com.mozylee.book.springboot.domain.posts.PostsRepository;
@@ -18,10 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
