17533b51cd6eca00ed0b297129a498346ba4e2e2
Mozy Lee <whyalwaysmeyy@gmail.com>
2022-09-03 10:21:57 +0900

    5.6 네이버 로그인 추가
    - ofNaver 추가
    - 화면에 네이버 로그인 추가

--- src/main/java/com/mozylee/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/mozylee/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,7 +25,24 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        // Naver Response의 형태 때문에 한 단계 더 거쳐야 함
+        // Naver Response의 최상위 Attribute는 Response, resultcode, message로 이루어져 있고 Response에 필요한 값들이 존재
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
